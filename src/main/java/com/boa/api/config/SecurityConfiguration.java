c78b10b07b76262883d96ece6d3d2851d8033864
Tidiane <tijdrame@gmail.com>
2021-01-05 13:39:26 +0100

    pdf bis non necessaire

--- src/main/java/com/boa/api/config/SecurityConfiguration.java
+++ src/main/java/com/boa/api/config/SecurityConfiguration.java
@@ -44,6 +44,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) {
         web.ignoring()
             .antMatchers(HttpMethod.OPTIONS, "/**")
+            //.antMatchers("static/**")
             .antMatchers("/swagger-ui/index.html")
             .antMatchers("/test/**");
     }
